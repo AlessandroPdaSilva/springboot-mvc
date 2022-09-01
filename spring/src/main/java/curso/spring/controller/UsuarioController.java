0b88f39d60f6ca6144a04f8d9888c4373d0c0ee1
alessandro.silva <alessandro.silva@ESTACAO.SETUR.DF.GOV.BR>
2022-09-01 17:39:28 -0300

    Criando edição de usuario

--- spring/src/main/java/curso/spring/controller/UsuarioController.java
+++ spring/src/main/java/curso/spring/controller/UsuarioController.java
@@ -1,7 +1,11 @@
 package curso.spring.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,8 +20,12 @@ public class UsuarioController {
 	private UsuarioRepository usuarioRepository;
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastrousuario")
-	public String inicio(){
-		return "cadastro/cadastrousuario";
+	public ModelAndView inicio(){
+		
+		ModelAndView mv = new ModelAndView("cadastro/cadastrousuario");
+		mv.addObject("usuarioeditar", new Usuario());
+		
+		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/salvarusuario")
@@ -37,9 +45,23 @@ public class UsuarioController {
 		Iterable<Usuario> u = usuarioRepository.findAll();
 		
 		mv.addObject("listaUsuario", u);
+		mv.addObject("usuarioeditar", new Usuario());
 		
 		return mv;
 	}
 	
+	
+	@GetMapping(value = "/editarusuario/{idusuario}")
+	public ModelAndView editarUsuario(@PathVariable(value = "idusuario") Long idUsuario){
+		
+		ModelAndView mv = new ModelAndView("cadastro/cadastrousuario");
+		
+		Optional<Usuario> aux = usuarioRepository.findById(idUsuario);
+		Usuario u = aux.get();
+		
+		mv.addObject("usuarioeditar", u); 
+		
+		return listarUsuarios();
+	}
 	
 }
